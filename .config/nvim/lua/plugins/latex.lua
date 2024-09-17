65eb1970b26513455bf80fb234042ba9aabb96e0
Alexis Sabourin <155196582+alexissabourin@users.noreply.github.com>
2024-09-17 13:30:53 -0400

    feat(nvim): add latex setup

---
+++ .config/nvim/lua/plugins/latex.lua
@@ -0,0 +1,7 @@
+return {
+	"lervag/vimtex",
+	lazy = false,
+	init = function()
+		vim.g.vimtex_view_general_viewer = "/mnt/c/SumatraPDF/SumatraPDF.exe"
+	end,
+}
--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -16,6 +16,7 @@ return {
 					"bashls",
 					"taplo",
 					"eslint",
+					"texlab",
 				},
 			},
 		},
